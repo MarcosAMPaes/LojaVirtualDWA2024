e19fb7fc847ad1c543fe86dcf6f546807308bed4
Marcos Antonio <marcosampaes@gmail.com>
2024-09-11 16:07:10 -0300

    criado endpoint de autenticação para o admin

--- routes/admin_routes.py
+++ routes/admin_routes.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter, Depends, Path
 from fastapi.responses import JSONResponse
 from dtos.alterar_pedido_dto import AlterarPedidoDto
 from dtos.alterar_produto_dto import AlterarProdutoDto
+from dtos.entrar_dto import EntrarDto
 from dtos.id_produto_dto import IdProdutoDTO
 from dtos.inserir_produto_dto import InserirProdutoDTO
 from dtos.problem_detail_dto import ProblemDetailsDto
@@ -10,6 +11,9 @@ from models.pedido_model import EstadoPedido
 from models.produto_model import Produto
 from repositories.pedido_repo import PedidoRepo
 from repositories.produto_repo import ProdutoRepo
+from repositories.usuario_repo import UsuarioRepo
+from util.auth_jwt import conferir_senha, criar_token
+from util.pydantic import create_validation_errors
 
 
 router = APIRouter(prefix="/manager")
@@ -70,3 +74,18 @@ async def obter_pedidos_por_estado(estado: EstadoPedido = Path(..., title="Id do
     if pedidos: return pedidos
     pd = ProblemDetailsDto("int", f"O pedido com id {estado} não foi encontrado", "value_not_found", ["body", "id_produto"])
     return JSONResponse(pd.to_dict(), status_code = 404)
+
+
+@router.post("/entrar")
+async def entrar(entrar_dto: EntrarDto):
+    cliente_entrou = UsuarioRepo.obter_por_email(entrar_dto.email)
+    if (
+        (not cliente_entrou)
+        or (not cliente_entrou.senha)
+        or (not conferir_senha(entrar_dto.senha, cliente_entrou.senha))
+    ):
+        pd = ProblemDetailsDto("str", f"Credenciais inválidas, certifique-se que está cadastrado e de que sua senha está correta.", "value_not_found", ["body", "email", "senha"])
+        return JSONResponse(pd.to_dict(), status_code = 404)
+
+    token = criar_token(cliente_entrou.id, cliente_entrou.nome, cliente_entrou.email, cliente_entrou.perfil)
+    return JSONResponse({"token": token})
